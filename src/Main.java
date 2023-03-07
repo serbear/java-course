64b24f8cf441c6a552ec2a436c6dbcd8dd72e90b
Sergei Markov <smarkov.dev@gmail.com>
2023-03-07 12:15:28 +0200

    clean struct

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,8 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        // Ex 1. Sum Calculator
-        testSimpleCalculator();
-        // Ex 2. Carpet Cost Calculator
-        testCarpetCost();
-        // Ex 3. Complex Operations
-        testComplexOperations();
-        // Ex 4. Cylinder
-        testCylinder();
-        // Ex 5. Pool Area
-        testPoolArea();
-    }
 
-    private static void testPoolArea() {
-        Rectangle rectangle = new Rectangle(5, 10);
-        System.out.println("rectangle.width= " + rectangle.getWidth());
-        System.out.println("rectangle.length= " + rectangle.getLength());
-        System.out.println("rectangle.area= " + rectangle.getArea());
-        Cuboid cuboid = new Cuboid(5,10,5);
-        System.out.println("cuboid.width= " + cuboid.getWidth());
-        System.out.println("cuboid.length= " + cuboid.getLength());
-        System.out.println("cuboid.area= " + cuboid.getArea());
-        System.out.println("cuboid.height= " + cuboid.getHeight());
-        System.out.println("cuboid.volume= " + cuboid.getVolume());
-    }
-
-    private static void testCylinder() {
-        Circle circle = new Circle(3.75);
-        System.out.println("circle.radius= " + circle.getRadius());
-        System.out.println("circle.area= " + circle.getArea());
-        Cylinder cylinder = new Cylinder(5.55, 7.25);
-        System.out.println("cylinder.radius= " + cylinder.getRadius());
-        System.out.println("cylinder.height= " + cylinder.getHeight());
-        System.out.println("cylinder.area= " + cylinder.getArea());
-        System.out.println("cylinder.volume= " + cylinder.getVolume());
-    }
-
-    private static void testComplexOperations() {
-        ComplexNumber one = new ComplexNumber(1.0, 1.0);
-        ComplexNumber number = new ComplexNumber(2.5, -1.5);
-        one.add(1,1);
-        System.out.println("one.real= " + one.getReal());
-        System.out.println("one.imaginary= " + one.getImaginary());
-        one.subtract(number);
-        System.out.println("one.real= " + one.getReal());
-        System.out.println("one.imaginary= " + one.getImaginary());
-        number.subtract(one);
-        System.out.println("number.real= " + number.getReal());
-        System.out.println("number.imaginary= " + number.getImaginary());
-    }
-
-    private static void testSimpleCalculator() {
-        SimpleCalculator calculator = new SimpleCalculator();
-        calculator.setFirstNumber(5.0);
-        calculator.setSecondNumber(4);
-        System.out.println("add= " + calculator.getAdditionResult());
-        System.out.println("subtract= " + calculator.getSubtractionResult());
-        calculator.setFirstNumber(5.25);
-        calculator.setSecondNumber(0);
-        System.out.println("multiply= " + calculator.getMultiplicationResult());
-        System.out.println("divide= " + calculator.getDivisionResult());
-    }
-    private static void testCarpetCost() {
-        Carpet carpet = new Carpet(3.5);
-        Floor floor = new Floor(2.75, 4.0);
-        Calculator calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
-        carpet = new Carpet(1.5);
-        floor = new Floor(5.4, 4.5);
-        calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
     }
 
 }
